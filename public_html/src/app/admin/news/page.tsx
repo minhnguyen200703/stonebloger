2db59caeaa72ae88eb61cd8525bb3f55a11d197e
Minh Nguyen <101308871+minhnguyen200703@users.noreply.github.com>
2024-12-06 09:29:18 +0700

    bugs in page content css

--- public_html/src/app/admin/news/page.tsx
+++ public_html/src/app/admin/news/page.tsx
@@ -138,7 +138,7 @@ const Admin = () => {
 					<Link href="/admin/news">Post</Link>
 				</Breadcrumb.Item>
 			</Breadcrumb>
-			<PageContent style={{ marginLeft: "-100px", paddingLeft: "50px" }}>
+			<PageContent className="ml-[-100px] pl-[50px]">
 				<Title level={3}>Posts</Title>
 				<div className="flex justify-end mb-3">
 					<Button
