8a294394c48bed4454cb0ccfa931b858990fec37
Minh Nguyen <101308871+minhnguyen200703@users.noreply.github.com>
2025-01-13 08:48:06 +0700

    Edit content

--- public_html/src/constant/data/members.ts
+++ public_html/src/constant/data/members.ts
@@ -17,48 +17,33 @@ export const members: Personal[] = [
       "Incubator Program",
     ],
     name: "Rodney Stone",
-    imageUrl: "/images/members/member1.png",
+    imageUrl: "/images/members/RodneyStone.png",
   },
   {
     id: "2",
     region: "Australia",
     services: ["Business Advisory", "Accounting", "Taxation"],
     name: "Lydia Atwell",
-    imageUrl: "/images/members/member1.png",
+    imageUrl: "/images/members/LydiaAtwell.png",
   },
   {
     id: "3",
     region: "Portland, Victoria",
     services: ["Business Advisory", "Accounting", "Taxation"],
     name: "Paul Wright",
-    imageUrl: "/images/members/member1.png",
+    imageUrl: "/images/members/PaulWright.png",
   },
   {
     id: "4",
-    region: "South East Asia",
+    region: "Sunshine Coast, Queensland",
     services: [
       "Business Advisory",
       "Accounting",
-      "Taxation",
-      "Incubator Program",
+      "Taxation"
     ],
-    name: "Rodney Stone",
-    imageUrl: "/images/members/member1.png",
-  },
-  {
-    id: "5",
-    region: "Australia",
-    services: ["Business Advisory", "Accounting", "Taxation"],
-    name: "Lydia Atwell",
-    imageUrl: "/images/members/member1.png",
-  },
-  {
-    id: "6",
-    region: "Portland, Victoria",
-    services: ["Business Advisory", "Accounting", "Taxation"],
-    name: "Paul Wright",
-    imageUrl: "/images/members/member1.png",
-  },
+    name: "Daren Cardow",
+    imageUrl: "/images/members/DarenCardow.png",
+  }
 ];
 
 export interface ProfileData {
@@ -93,7 +78,7 @@ export const profileDatas: ProfileData[] = [
       phone: "+84 938 370 432",
       email: "rodney@stoneaccounting.com.au",
       address:
-        "64/12 Street 34, An Khanh Ward, Thu Duc City, Ho Chi Minh City, Vietnam",
+        "A. 64/12 Street 34, An Khanh Ward, Thu Duc City, Ho Chi Minh City, Vietnam",
     },
     services: [
       "Business Advisory",
@@ -104,52 +89,73 @@ export const profileDatas: ProfileData[] = [
     ],
     certificates: ["CA", "B. Acc", "AD (Fin Plan)", "Registered Tax Agent"],
     office: "South East Asia",
-    operatingHours: "9 AM - 5 PM, Monday to Friday",
-    linkedInUrl: "https://linkedin.com/in/rodney-stone", // Replace with actual LinkedIn URL
-    imageUrl: "/images/members/member1.png", // Replace with actual image URL
+    operatingHours: "9 AM - 5 PM \n Monday to Friday",
+    linkedInUrl: "https://www.linkedin.com/in/rodney-stone/", // Replace with actual LinkedIn URL
+    imageUrl: "/images/members/RodneyStone.png", // Replace with actual image URL
     location:
-      "https://www.google.com/maps/d/u/0/embed?mid=1uf7LJKbCDQ9MB5gz80mKeOUr_6Qs3pk&ehbc=2E312F",
+      "https://www.google.com/maps/d/embed?mid=1J5EaBau34rboyRDas__GfD76-3fedp4&ehbc=2E312F",
   },
   {
     id: "2",
     name: "Lydia Atwell",
     title: "Executive Director",
-    region: "Australia",
+    region: "Portland, Victoria (Head Office)",
     description:
-      "Lydia Atwell is an Executive Director at STONE Accounting Group, focusing on business advisory and taxation services in Australia. With expertise in accounting, she is committed to helping businesses navigate financial challenges and optimize their operations.",
+      "Lydia Atwell serves as the Executive Director at STONE Accounting Group, responsible for the firm's operations in Australia, with the head office located in Portland, Victoria. Lydia is committed to fostering client success through innovative accounting solutions and personalized advisory services, ensuring that STONE Accounting Group remains a trusted partner for businesses across Australia and beyond.",
     contactDetails: {
-      phone: "+61 3 9876 5432",
+      phone: "+61 411 427 713",
       email: "lydia@stoneaccounting.com.au",
-      address: "10/25 Collins St, Melbourne, Victoria, Australia",
+      address: "A. 125b Percy Street, Portland, Victoria",
     },
     services: ["Business Advisory", "Accounting", "Taxation"],
-    certificates: ["CA", "B. Acc", "Registered Tax Agent"],
-    office: "Australia",
-    operatingHours: "9 AM - 5 PM, Monday to Friday",
-    linkedInUrl: "https://linkedin.com/in/lydia-atwell", // Replace with actual LinkedIn URL
-    imageUrl: "/images/members/member1.png", // Replace with actual image URL
+    certificates: ["CA", "B. Com"],
+    office: "Portland, Victoria (Head Office)",
+    operatingHours: "9 AM - 5 PM\nMonday to Friday",
+    linkedInUrl: "https://www.linkedin.com/in/lydia-atwell", // Replace with actual LinkedIn URL
+    imageUrl: "/images/members/LydiaAtwell.png", // Replace with actual image URL
     location:
-      "https://www.google.com/maps/d/u/0/embed?mid=1uf7LJKbCDQ9MB5gz80mKeOUr_6Qs3pk&ehbc=2E312F",
+      "https://www.google.com/maps/d/embed?mid=1U9bkfF-f2v2J_5rGSkP8F4eeBfRqxjc&ehbc=2E312F",
   },
   {
     id: "3",
     name: "Paul Wright",
-    title: "Executive Director",
+    title: "Senior Accountant",
     region: "Portland, Victoria",
     description:
-      "Paul Wright serves as the Executive Director at STONE Accounting Group, overseeing operations in Portland, Victoria. He brings years of experience in accounting and taxation, helping local businesses manage their finances and grow in the competitive market.",
+      "Paul has consistently demonstrated that he's a rock-solid professional with a deep understanding of our core values. Backed by his extensive experience and a solid professional network, Paul is prepared to roll out top-tier services in tax preparation, bookkeeping, financial accounting, and business advisory. With years of experience, Paul’s dedication and readiness ensure that his clients are in capable hands.",
     contactDetails: {
-      phone: "+61 3 4567 8901",
+      phone: "+61 3 5523 1234",
       email: "paul@stoneaccounting.com.au",
-      address: "12 Main St, Portland, Victoria, Australia",
+      address: "A. 1b 111 Bentinck Street, Portland, Victoria ",
     },
     services: ["Business Advisory", "Accounting", "Taxation"],
-    certificates: ["CA", "B. Acc", "Registered Tax Agent"],
+    certificates: ["B. Com"],
     office: "Portland, Victoria",
-    operatingHours: "9 AM - 5 PM, Monday to Friday",
-    linkedInUrl: "https://linkedin.com/in/paul-wright", // Replace with actual LinkedIn URL
-    imageUrl: "/images/members/member1.png", // Replace with actual image URL
+    operatingHours: "9 AM - 5 PM\nMonday to Friday",
+    linkedInUrl: "https://www.linkedin.com/in/wrightrp/ ", // Replace with actual LinkedIn URL
+    imageUrl: "/images/members/PaulWright.png", // Replace with actual image URL
     location:
-      "https://www.google.com/maps/d/u/0/embed?mid=1uf7LJKbCDQ9MB5gz80mKeOUr_6Qs3pk&ehbc=2E312F",
+      "https://www.google.com/maps/d/embed?mid=1b_ZXGTM-eNegYe4wjX5WOcwwnHjOlY8&ehbc=2E312F",
+  },
+  {
+    id: "4",
+    name: "Daren Cardow",
+    title: "Senior Accountant",
+    region: "Sunshine Coast, Queensland",
+    description:
+      "With his reknowned expertise, a stellar local reputation, and unparalleled knowledge of the Sunshine Coast’s unique business environment, Daren is a natural fit for the STONE network. Backed by our innovative and extensive resources and commitment to exceptional service, Daren is set to elevate the client experience for businesses and individuals across the region.",
+    contactDetails: {
+      phone: "+61 414 359 783",
+      email: "daren@stoneaccounting.com.au",
+      address: "A. Sunshine Coast, Queensland",
+    },
+    services: ["Business Advisory", "Accounting", "Taxation"],
+    certificates: ["CPA", "GAIDC", "Registered Tax Agent", "B. Bus"],
+    office: "Sunshine Coast, Queensland",
+    operatingHours: "9 AM - 5 PM\nMonday to Friday",
+    linkedInUrl: "https://www.linkedin.com/in/daren-cardow-91039031/", // Replace with actual LinkedIn URL
+    imageUrl: "/images/members/DarenCardow.png", // Replace with actual image URL
+    location:
+      "https://www.google.com/maps/d/embed?mid=1b_ZXGTM-eNegYe4wjX5WOcwwnHjOlY8&ehbc=2E312F",
   },
 ];
